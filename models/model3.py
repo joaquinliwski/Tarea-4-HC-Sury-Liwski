598487852c7377054a29f96255844ecf459ca125
Camila Sury <camisury@gmail.com>
2022-07-18 17:26:59 -0300

    model3

--- models/model3.py
+++ models/model3.py
@@ -8,42 +8,20 @@ With QGIS : 32208
 from qgis.core import QgsProcessing
 from qgis.core import QgsProcessingAlgorithm
 from qgis.core import QgsProcessingMultiStepFeedback
-from qgis.core import QgsProcessingParameterFeatureSink
 import processing
 
 
 class Model3(QgsProcessingAlgorithm):
 
     def initAlgorithm(self, config=None):
-        self.addParameter(QgsProcessingParameterFeatureSink('Drop_fields_3', 'drop_fields_3', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
-        self.addParameter(QgsProcessingParameterFeatureSink('Fixgeo_3', 'fixgeo_3', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
-        self.addParameter(QgsProcessingParameterFeatureSink('Landq', 'landq', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
-        self.addParameter(QgsProcessingParameterFeatureSink('Pop1800', 'pop1800', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
-        self.addParameter(QgsProcessingParameterFeatureSink('Pop1900', 'pop1900', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
-        self.addParameter(QgsProcessingParameterFeatureSink('Pop2000', 'pop2000', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        pass
 
     def processAlgorithm(self, parameters, context, model_feedback):
         # Use a multi-step feedback, so that individual child algorithm progress reports are adjusted for the
         # overall progress through the model
-        feedback = QgsProcessingMultiStepFeedback(7, model_feedback)
+        feedback = QgsProcessingMultiStepFeedback(1, model_feedback)
         results = {}
         outputs = {}
-
-        # Zonal statistics
-        alg_params = {
-            'COLUMN_PREFIX': 'pop2000',
-            'INPUT': 'Zonal_Statistics_e6e976d5_5184_408a_aee7_eb8a167d0fd8',
-            'INPUT_RASTER': 'popd_2000AD_79abad50_949e_4b1a_89f2_3828258fd12d',
-            'RASTER_BAND': 1,
-            'STATISTICS': [2],  # Mean
-            'OUTPUT': parameters['Pop2000']
-        }
-        outputs['ZonalStatistics'] = processing.run('native:zonalstatisticsfb', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Pop2000'] = outputs['ZonalStatistics']['OUTPUT']
-
-        feedback.setCurrentStep(1)
-        if feedback.isCanceled():
-            return {}
 
         # Save vector features to file
         alg_params = {
@@ -51,83 +29,10 @@ class Model3(QgsProcessingAlgorithm):
             'INPUT': 'Zonal_Statistics_a0941aaf_d5b2_4381_acf6_39f2ca944e5b',
             'LAYER_NAME': '',
             'LAYER_OPTIONS': '',
-            'OUTPUT': '/Users/camilasury/Desktop/Herramientas computacionales/TP2/raster_stast.gpkg',
+            'OUTPUT': '/Users/camilasury/Desktop/Herramientas computacionales/Python & QGIS/Output/raster_stats.csv',
             'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
         }
         outputs['SaveVectorFeaturesToFile'] = processing.run('native:savefeatures', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-
-        feedback.setCurrentStep(2)
-        if feedback.isCanceled():
-            return {}
-
-        # Zonal statistics
-        alg_params = {
-            'COLUMN_PREFIX': 'pop1800',
-            'INPUT': 'Zonal_Statistics_9c9e8c03_781d_443f_86d3_cd1de473d0d3',
-            'INPUT_RASTER': 'popd_1800AD_1e9d99da_c7b4_4557_b31b_ed22669a5bcf',
-            'RASTER_BAND': 1,
-            'STATISTICS': [2],  # Mean
-            'OUTPUT': parameters['Pop1800']
-        }
-        outputs['ZonalStatistics'] = processing.run('native:zonalstatisticsfb', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Pop1800'] = outputs['ZonalStatistics']['OUTPUT']
-
-        feedback.setCurrentStep(3)
-        if feedback.isCanceled():
-            return {}
-
-        # Fix geometries
-        alg_params = {
-            'INPUT': '/Users/camilasury/Desktop/Herramientas computacionales/Python & QGIS/Input/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp',
-            'OUTPUT': parameters['Fixgeo_3']
-        }
-        outputs['FixGeometries'] = processing.run('native:fixgeometries', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Fixgeo_3'] = outputs['FixGeometries']['OUTPUT']
-
-        feedback.setCurrentStep(4)
-        if feedback.isCanceled():
-            return {}
-
-        # Drop field(s)
-        alg_params = {
-            'COLUMN': ['featurecla','scalerank','LABELRANK','SOVEREIGNT','SOV_A3','ADM0_DIF','LEVEL','TYPE','TLC','ADM0_A3','GEOU_DIF','GEOUNIT','GU_A3','SU_DIF','SUBUNIT','SU_A3','BRK_DIFF','NAME','NAME_LONG','BRK_A3','BRK_NAME','BRK_GROUP','ABBREV','POSTAL','FORMAL_EN','FORMAL_FR','NAME_CIAWF','NOTE_ADM0','NOTE_BRK','NAME_SORT','NAME_ALT','MAPCOLOR7','MAPCOLOR8','MAPCOLOR9','MAPCOLOR13','POP_EST','POP_RANK','POP_YEAR','GDP_MD','GDP_YEAR','ECONOMY','INCOME_GRP','FIPS_10','ISO_A2','ISO_A2_EH','ISO_A3_EH','ISO_N3','ISO_N3_EH','UN_A3','WB_A2','WB_A3','WOE_ID','WOE_ID_EH','WOE_NOTE','ADM0_ISO','ADM0_DIFF','ADM0_TLC','ADM0_A3_US','ADM0_A3_FR','ADM0_A3_RU','ADM0_A3_ES','ADM0_A3_CN','ADM0_A3_TW','ADM0_A3_IN','ADM0_A3_NP','ADM0_A3_PK','ADM0_A3_DE','ADM0_A3_GB','ADM0_A3_BR','ADM0_A3_IL','ADM0_A3_PS','ADM0_A3_SA','ADM0_A3_EG','ADM0_A3_MA','ADM0_A3_PT','ADM0_A3_AR','ADM0_A3_JP','ADM0_A3_KO','ADM0_A3_VN','ADM0_A3_TR','ADM0_A3_ID','ADM0_A3_PL','ADM0_A3_GR','ADM0_A3_IT','ADM0_A3_NL','ADM0_A3_SE','ADM0_A3_BD','ADM0_A3_UA','ADM0_A3_UN','ADM0_A3_WB','CONTINENT','REGION_UN','SUBREGION','REGION_WB','NAME_LEN','LONG_LEN','ABBREV_LEN','TINY','HOMEPART','MIN_ZOOM','MIN_LABEL','MAX_LABEL','LABEL_X','LABEL_Y','NE_ID','WIKIDATAID','NAME_AR','NAME_BN','NAME_DE','NAME_EN','NAME_ES','NAME_FA','NAME_FR','NAME_EL','NAME_HE','NAME_HI','NAME_HU','NAME_ID','NAME_IT','NAME_JA','NAME_KO','NAME_NL','NAME_PL','NAME_PT','NAME_RU','NAME_SV','NAME_TR','NAME_UK','NAME_UR','NAME_VI','NAME_ZH','NAME_ZHT','FCLASS_ISO','TLC_DIFF','FCLASS_TLC','FCLASS_US','FCLASS_FR','FCLASS_RU','FCLASS_ES','FCLASS_CN','FCLASS_TW','FCLASS_IN','FCLASS_NP','FCLASS_PK','FCLASS_DE','FCLASS_GB','FCLASS_BR','FCLASS_IL','FCLASS_PS','FCLASS_SA','FCLASS_EG','FCLASS_MA','FCLASS_PT','FCLASS_AR','FCLASS_JP','FCLASS_KO','FCLASS_VN','FCLASS_TR','FCLASS_ID','FCLASS_PL','FCLASS_GR','FCLASS_IT','FCLASS_NL','FCLASS_SE','FCLASS_BD','FCLASS_UA'],
-            'INPUT': 'Fixed_geometries_0bd1ad9e_4e6b_482a_91de_abd54bb5f039',
-            'OUTPUT': parameters['Drop_fields_3']
-        }
-        outputs['DropFields'] = processing.run('native:deletecolumn', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Drop_fields_3'] = outputs['DropFields']['OUTPUT']
-
-        feedback.setCurrentStep(5)
-        if feedback.isCanceled():
-            return {}
-
-        # Zonal statistics
-        alg_params = {
-            'COLUMN_PREFIX': '_',
-            'INPUT': 'Remaining_fields_1efc1236_5ed2_41f6_b173_8f607fc68577',
-            'INPUT_RASTER': 'landquality_5b32bbbd_2181_4afc_bdeb_50443f09b1e2',
-            'RASTER_BAND': 1,
-            'STATISTICS': [2],  # Mean
-            'OUTPUT': parameters['Landq']
-        }
-        outputs['ZonalStatistics'] = processing.run('native:zonalstatisticsfb', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Landq'] = outputs['ZonalStatistics']['OUTPUT']
-
-        feedback.setCurrentStep(6)
-        if feedback.isCanceled():
-            return {}
-
-        # Zonal statistics
-        alg_params = {
-            'COLUMN_PREFIX': 'pop1900',
-            'INPUT': 'Zonal_Statistics_e6e976d5_5184_408a_aee7_eb8a167d0fd8',
-            'INPUT_RASTER': 'popd_1900AD_84dc9588_658f_4614_b603_abf941e71fb9',
-            'RASTER_BAND': 1,
-            'STATISTICS': [2],  # Mean
-            'OUTPUT': parameters['Pop1900']
-        }
-        outputs['ZonalStatistics'] = processing.run('native:zonalstatisticsfb', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Pop1900'] = outputs['ZonalStatistics']['OUTPUT']
         return results
 
     def name(self):
