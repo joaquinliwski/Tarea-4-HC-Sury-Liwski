da4b2a6782c482f53e88630ff6eb1a2ebbc2e847
Camila Sury <camisury@gmail.com>
2022-07-18 17:19:03 -0300

    corrección modelos

--- models/model1.py
+++ models/model1.py
@@ -5,24 +5,87 @@ Group :
 With QGIS : 32208
 """
 
-from qgis.core import QgsProcessing                     #
-from qgis.core import QgsProcessingAlgorithm            # Importamos algunas funciones esppecificas del modulo gis.core
-from qgis.core import QgsProcessingMultiStepFeedback    #
-from qgis.core import QgsProcessingParameterFeatureSink #
-import processing                                       # Imporamos paquete que ayuda procesos con APIs
+from qgis.core import QgsProcessing
+from qgis.core import QgsProcessingAlgorithm
+from qgis.core import QgsProcessingMultiStepFeedback
+from qgis.core import QgsProcessingParameterFeatureSink
+import processing
 
 
 class Model1(QgsProcessingAlgorithm):
 
     def initAlgorithm(self, config=None):
+        self.addParameter(QgsProcessingParameterFeatureSink('Autoinc_id', 'autoinc_id', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
         self.addParameter(QgsProcessingParameterFeatureSink('Wldsout', 'wldsout', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Length', 'length', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Field_calc', 'field_calc', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Output_menor_a_11', 'OUTPUT_menor_a_11', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Fix_geo', 'fix_geo', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
 
     def processAlgorithm(self, parameters, context, model_feedback):
         # Use a multi-step feedback, so that individual child algorithm progress reports are adjusted for the
         # overall progress through the model
-        feedback = QgsProcessingMultiStepFeedback(1, model_feedback)
+        feedback = QgsProcessingMultiStepFeedback(6, model_feedback)
         results = {}
         outputs = {}
+
+        # Field calculator clone
+        alg_params = {
+            'FIELD_LENGTH': 10,
+            'FIELD_NAME': 'lnm',
+            'FIELD_PRECISION': 0,
+            'FIELD_TYPE': 2,  # String
+            'FORMULA': '"NAME_PROP"',
+            'INPUT': 'menor_a_11_eb436653_bc8e_4926_a5b7_d3a3f23de8f4',
+            'OUTPUT': parameters['Field_calc']
+        }
+        outputs['FieldCalculatorClone'] = processing.run('native:fieldcalculator', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Field_calc'] = outputs['FieldCalculatorClone']['OUTPUT']
+
+        feedback.setCurrentStep(1)
+        if feedback.isCanceled():
+            return {}
+
+        # Field calculator
+        alg_params = {
+            'FIELD_LENGTH': 2,
+            'FIELD_NAME': 'length',
+            'FIELD_PRECISION': 0,
+            'FIELD_TYPE': 1,  # Integer
+            'FORMULA': 'length(NAME_PROP)',
+            'INPUT': 'Incremented_db0f2cff_6da4_4320_a14e_ededf985a311',
+            'OUTPUT': parameters['Length']
+        }
+        outputs['FieldCalculator'] = processing.run('native:fieldcalculator', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Length'] = outputs['FieldCalculator']['OUTPUT']
+
+        feedback.setCurrentStep(2)
+        if feedback.isCanceled():
+            return {}
+
+        # Feature filter
+        alg_params = {
+            'INPUT': 'Calculated_f6b3724d_dd57_4528_8f50_1523df3951a1',
+            'OUTPUT_menor_a_11': parameters['Output_menor_a_11']
+        }
+        outputs['FeatureFilter'] = processing.run('native:filter', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Output_menor_a_11'] = outputs['FeatureFilter']['OUTPUT_menor_a_11']
+
+        feedback.setCurrentStep(3)
+        if feedback.isCanceled():
+            return {}
+
+        # Fix geometries
+        alg_params = {
+            'INPUT': '/Users/camilasury/Desktop/Herramientas computacionales/Python & QGIS/Input/langa/langa.shp',
+            'OUTPUT': parameters['Fix_geo']
+        }
+        outputs['FixGeometries'] = processing.run('native:fixgeometries', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Fix_geo'] = outputs['FixGeometries']['OUTPUT']
+
+        feedback.setCurrentStep(4)
+        if feedback.isCanceled():
+            return {}
 
         # Drop field(s)
         alg_params = {
@@ -32,6 +95,25 @@ class Model1(QgsProcessingAlgorithm):
         }
         outputs['DropFields'] = processing.run('native:deletecolumn', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
         results['Wldsout'] = outputs['DropFields']['OUTPUT']
+
+        feedback.setCurrentStep(5)
+        if feedback.isCanceled():
+            return {}
+
+        # Add autoincremental field
+        alg_params = {
+            'FIELD_NAME': 'GID',
+            'GROUP_FIELDS': [''],
+            'INPUT': outputs['FixGeometries']['OUTPUT'],
+            'MODULUS': 0,
+            'SORT_ASCENDING': True,
+            'SORT_EXPRESSION': '',
+            'SORT_NULLS_FIRST': False,
+            'START': 1,
+            'OUTPUT': parameters['Autoinc_id']
+        }
+        outputs['AddAutoincrementalField'] = processing.run('native:addautoincrementalfield', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Autoinc_id'] = outputs['AddAutoincrementalField']['OUTPUT']
         return results
 
     def name(self):
